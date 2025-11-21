3960c03ed4786f6c5d5850fb23fb7a13509e2361
Ali Abdelmoaty <abdelmoaty841@gmail.com>
2025-11-22 01:17:53 +0200

    Implement initial project structure with analysis options, routing, data sources, and state management for demo feature

--- lib/core/routing/app_router.dart
+++ lib/core/routing/app_router.dart
@@ -1,4 +1,6 @@
-import 'package:fintech_app/features/demo/ui/demo_screen.dart';
+// TODO: Do NOT add screen imports here. Add them in routes_exports.dart instead.
+
+import 'routes_exports.dart';
 import 'package:go_router/go_router.dart';
 
 final GoRouter appRouter = GoRouter(
