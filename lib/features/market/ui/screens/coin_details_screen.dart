9a48beb31b4e8bec1d9b9011cca9649fca3d533b
Ali Abdelmoaty <abdelmoaty841@gmail.com>
2025-11-25 06:55:46 +0200

    Remove unused theme toggle functionality from CoinDetailsScreen; cleaned up the app bar by eliminating the theme switch button, streamlining the layout for improved user experience.

--- lib/features/market/ui/screens/coin_details_screen.dart
+++ lib/features/market/ui/screens/coin_details_screen.dart
@@ -1,7 +1,6 @@
 import 'package:easy_localization/easy_localization.dart';
 import 'package:fintech_app/core/extensions/theme_extension.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:go_router/go_router.dart';
 
@@ -9,7 +8,6 @@ import '../../../../core/constant/translation_keys.dart';
 import '../../../../core/theme/app_colors.dart';
 import '../../../../core/theme/app_text_styles.dart';
 
-import '../../../../core/theme/cubit/theme_cubit.dart';
 import '../widgets/about_coin_details.dart';
 import '../widgets/crypto_chart.dart';
 import '../widgets/row_name_details.dart';
@@ -28,17 +26,6 @@ class CoinDetailsScreen extends StatelessWidget {
           color: isDark ? AppColors.white : AppColors.mainColor,
         ),
         title: Text(TranslationKeys.coinDetails.tr()),
-        actions: [
-          IconButton(
-            onPressed: () {
-              context.read<ThemeCubit>().toggleTheme(isDark);
-            },
-            icon: Icon(
-              Icons.dark_mode,
-              color: isDark ? AppColors.white : AppColors.mainColor,
-            ),
-          ),
-        ],
         centerTitle: true,
         leading: IconButton(
           onPressed: () {
