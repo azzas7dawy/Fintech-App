69828c377ad5a614feca6c6084347f637ed6bde6
Ali Abdelmoaty <abdelmoaty841@gmail.com>
2025-11-28 19:23:52 +0200

    Remove unused import for cached_network_image in card_crypto_market.dart to streamline code and improve maintainability.

--- lib/features/market/ui/widgets/card_crypto_market.dart
+++ lib/features/market/ui/widgets/card_crypto_market.dart
@@ -1,4 +1,3 @@
-import 'package:cached_network_image/cached_network_image.dart';
 import 'package:fintech_app/core/theme/app_colors.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
